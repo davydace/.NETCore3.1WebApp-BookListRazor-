034a2cc652eeb5bff70c44ff6ddbf6305539e2dc
David Edokpayid <david.edokpayi@outlook.com>
2020-06-19 15:40:17 +0100

    Designed the create form

--- BookListRazor/Pages/BookList/Create.cshtml.cs
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BookListRazor.Pages.BookList
 {
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Book Book { get; set; }
         public void OnGet()
         {
+
         }
     }
 }
